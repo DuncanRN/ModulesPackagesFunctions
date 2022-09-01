9f10b024a582e9d79057e0febfa343e55d1c6e51
Duncan Robertson <knacknud@gmail.com>
2022-09-01 15:39:35 +0100

    1 and 2 and 1 extension

--- modules/task_list.py
+++ modules/task_list.py
@@ -13,25 +13,30 @@ tasks = [
 
 ## Get a list of uncompleted tasks
 def get_uncompleted_tasks(list):
-    
-    uncompleted_tasks =[]
+    return get_tasks_by_status(list, False)
 
-    for task in list:
-        if task["completed"] == False:
-            uncompleted_tasks.append(task)
+    
+    # uncompleted_tasks =[]
+
+    # for task in list:
+    #     if task["completed"] == False:
+    #         uncompleted_tasks.append(task)
 
     # print(uncompleted_tasks)
 
-    return uncompleted_tasks
+    
 
 
 ## Get a list of completed tasks
 def get_completed_tasks(list):
-    completed_tasks = []
-    for task in list:
-        if task["completed"] == True:
-            completed_tasks.append(task)
-    return completed_tasks
+
+    return get_tasks_by_status(list, True)
+
+    # completed_tasks = []
+    # for task in list:
+    #     if task["completed"] == True:
+    #         completed_tasks.append(task)
+    # return completed_tasks
 
 
 ## Get tasks where time_taken is at least a given time
@@ -56,7 +61,14 @@ def get_task_with_description(list, description):
 
 ## Get a list of tasks by status
 def get_tasks_by_status(list, status):
-    pass
+    task_status = []
+    for task in list:
+        print("in for loop ")
+        if task["completed"] == status:
+            task_status.append(task)
+            print("in the if statement successfully")
+
+    return task_status
 
 def mark_task_complete(task):
     task["completed"] = True
